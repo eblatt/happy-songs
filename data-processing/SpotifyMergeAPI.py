7e61baf799e669cc4c55d0ddaca328f29661f4de
Mindalyn <mindy_ng@brown.edu>
2019-04-03 15:19:37 -0400

    Add files via upload

--- data-processing/SpotifyMergeAPI.py
+++ data-processing/SpotifyMergeAPI.py
@@ -16,13 +16,13 @@ c.execute('DROP TABLE IF EXISTS "features";')
 
 #Create tables
 c.execute('''CREATE TABLE IF NOT EXISTS features(id VARCHAR PRIMARY KEY,
-    name VARCHAR, danceability REAL, energy REAL, key REAL, loudness REAL,
+    danceability REAL, energy REAL, key REAL, loudness REAL,
     mode REAL, speechiness REAL, acousticness REAL, instrumentalness REAL,
     liveness REAL, valence REAL, tempo REAL, duration REAL, time_sig REAL);''')
 conn.commit()
 
 #0 - ID
-#1 - name - dont have yet
+#1 - name - dont have yet // DONT NEED ANYMORE
 #2 - danceability 8
 #3 - energy 2
 #4 - key 0
@@ -40,7 +40,7 @@ conn.commit()
 
 def grab_features(ids):
 
-    sp = spotipy.Spotify(auth='BQCb3w75TyqNhZ3P1ct8gkLvlukfCkwdSqPmolsIrhrNyOPBSoBDvyaQ8QPMQ9qXat7JGHqmWSN3_JyvT3vyD8FDdlAZXRZQQSNmlKyv_6JeqQoANCdztZUzoW0z-IO0au4ENXNZWA42jeENurrShNs')
+    sp = spotipy.Spotify(auth='BQDZReErZM5jAZGP-BYQ_NQWqT76J1WaLyUXcsc_yRk8bgxcrrM0-8lwBj1XaEp1rYgbLXp3vOhRG8LStJRBHvgKVHcn3hAbiyKNVLWIpqw_zPqE6FvCLKK07MaS5nkSMtgM6TPLM8gt4BJEpzzXgy0')
 #
 #    #check 200 status code
 #
@@ -49,62 +49,48 @@ def grab_features(ids):
 
     feature_set = []
     for song in song_set:
-        song_features = []
-        name = 'placeholder'
-        song_features.extend((song['id'], name, song['danceability'], song['energy'], song['key'], song['loudness'], song['mode'], song['speechiness'], song['acousticness'], song['instrumentalness'], song['liveness'], song['valence'], song['tempo'], song['duration_ms'], song['time_signature']))
-        feature_set.append(song_features)
+        if song is not None:
+            c.execute('''INSERT INTO features (id,
+                danceability, energy, key, loudness,
+                mode, speechiness, acousticness, instrumentalness,
+                liveness, valence, tempo, duration, time_sig)
+                SELECT ?,?,?,?,?,?,?,?,?,?,?,?,?,?
+                WHERE NOT EXISTS (SELECT * FROM features WHERE id = ?)''', (song['id'], song['danceability'], song['energy'], song['key'], song['loudness'], song['mode'], song['speechiness'], song['acousticness'], song['instrumentalness'], song['liveness'], song['valence'], song['tempo'], song['duration_ms'], song['time_signature'], song['id']))
+            conn.commit()
+        else:
+            print("Song is Null:", song)
     return feature_set #list of lists. length 50. each sublist has features of each song
 
-#    feature_set = []
-#
-#    for i in range(10):
-#        song_features = []
-#        features = sp.audio_features(ids[i]) #sample '2ekn2ttSfGqwhhate0LSR0' this line causes id to change to "URL" and my access token keeps expiring
-#        features = json.dumps(features[0])
-#        features = json.loads(features)
-#        name = 'placeholder'
-#        song_features.extend((ids[i], name, features['danceability'], features['energy'], features['key'], features['loudness'], features['mode'], features['speechiness'], features['acousticness'], features['instrumentalness'], features['liveness'], features['valence'], features['tempo'], features['duration_ms'], features['time_signature']))
-#        feature_set.append(song_features)
-#    print(feature_set)
-#    return feature_set
-
-
 with open('data.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    data = list(csv.reader(csv_file, delimiter=','))
     line_count = 0
     ids = []
-
-    for row in csv_reader:
-        line = row[4].split('/')
-        #print(line)
-        song_id = line[len(line)-1] #string
-
-        #if ID is not already in the table:
-            #make API CALL to get the feature values
-            #print(song_id) #WHY IS IT GETTING CHANGED TO URL???
-
-        if len(song_id) == 22:
-            ids.append(song_id)
-            line_count += 1
+    feature_set = []
     
-        if line_count == 50:
-            feature_set_50 = grab_features(ids)
-            for song_features in feature_set_50:
-                c.execute('''INSERT INTO features (id,
-                    name, danceability, energy, key, loudness,
-                    mode, speechiness, acousticness, instrumentalness,
-                    liveness, valence, tempo, duration, time_sig)
-                    SELECT ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?
-                    WHERE NOT EXISTS (SELECT * FROM features WHERE id = ?)''', (song_features[0], song_features[1], song_features[2], song_features[3], song_features[4], song_features[5], song_features[6], song_features[7],song_features[8], song_features[9], song_features[10], song_features[11], song_features[12], song_features[13], song_features[14], song_features[0]))
-                conn.commit()
+    for row in range(1, len(data), 10): #start doing the skips at this level
+        line = data[row][4].split('/')
+        song_id = line[len(line)-1] #string
+        
+        if song_id and len(song_id) == 22:
+            line_count += 1
+            if line_count % 20 == 1: #every 20
+                ids.append(song_id)
+        else:
+            print("not valid:", song_id)
+    
+        #also update to 100 at a time
+        if line_count == 100:
+            feature_set = grab_features(ids)
             ids = []
             line_count = 0
-#if there isn't 50 at the end then it won't read the last few songs
-            #print("new 50")
 
+    #if there isn't 100 at the end, read the last few songs
+    feature_set = grab_features(ids)
 
-
-
+#song_features = []
+#            song_features.extend((song['id'], name, song['danceability'], song['energy'], song['key'], song['loudness'], song['mode'], song['speechiness'], song['acousticness'], song['instrumentalness'], song['liveness'], song['valence'], song['tempo'], song['duration_ms'], song['time_signature']))
+#feature_set.append(song_features)
+#if ID is not already in the table:
 
 #    #50 at a time
 #    counter = 0
